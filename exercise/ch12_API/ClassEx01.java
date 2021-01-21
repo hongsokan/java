d780516e126967c97efbb53218cedcf4118659df
hongsokan <hongsokan@daum.net>
2021-01-21 11:54:08 +0900

    0121

---
+++ exercise/ch12_API/ClassEx01.java
@@ -0,0 +1,31 @@
+package ch12_API;
+
+public class ClassEx01 {
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.num = 99;
+		System.out.println(s);
+		try {
+			// 변수 명으로 클래스를 만들어서 처리할 수 있다
+			Class<?> c = Class.forName("ch12_API.Sample");
+			Sample e = (Sample) c.newInstance();
+			e.num = 10;
+			System.out.println(e);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class Sample {
+	int num;
+	
+	@Override
+	public String toString() {
+		return num + "";
+	}
+}
